416cb800d773a2cee3a86a5869406e41646140aa
anovelexperience <bloodbank@dracula.email>
2020-04-17 02:32:17 +0100

    Add home and update menu item display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
+{{-- Authenticated users --}}
 @extends('layouts.dracula')
 
 @section('content')
     @include('default-banner')
+
     <section class="w-full py-4 px-6 mt-6 flex flex-col">
-        <h1>Epistolary Novels</h1>
-        <p>This should be a list of epistolary novels and a description of what they are.</p>
+        <h2>Welcome {{ auth()->user()->name }}!</h2>
     </section>
 @endsection
