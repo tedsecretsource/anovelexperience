52c66442d151e6ce7f4fef53e62416dd9acf7eee
anovelexperience <bloodbank@dracula.email>
2020-05-03 12:05:08 +0100

    Update default email address

--- config/anovelexperience.php
+++ config/anovelexperience.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'number_of_trial_chapters' => 10,
-    'receiver_email' => env('PAYPAL_MERCHANT_EMAIL', 'merchant@dracula.email'),
+    'receiver_email' => env('PAYPAL_MERCHANT_EMAIL', 'merchant@anovelexperience.email'),
     'paypal_token_lifetime' => 0,
     'paypal_token_refreshed' => '1970-01-01 00:00:00',
     'paypal_token' => '',
