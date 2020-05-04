69e7e13c1b548cdd3b92876befdbb3d6da3d87dd
anovelexperience <bloodbank@dracula.email>
2020-05-04 16:30:21 +0100

    Bug fix: entries were being sent at the wrong time
    Fixed an issue in which entries were never being sent because, well, the logic was messed up.

--- app/Subscription.php
+++ app/Subscription.php
@@ -57,12 +57,19 @@ class Subscription extends Model
     /**
      * Returns the pace-adjusted next entry delivery date
      *
+     * Method for calculating the next "normalized" send date is:
+     *
+     * 1. Find the difference between the previous entry and the current entry in seconds
+     * 2. Divide that by the pace
+     * 3. Add the seconds to the most recent log entry
+     *
      * @return object A Carbon date object consisting of the pace-adjusted next delivery date or null if we're at the end of the book
      */
     public function getNextEntryDeliveryDateAttribute()
     {
         $next_entry = $this->getNextEntry();
         if (null == $next_entry) {
+            // The last entry has already been sent
             // I could return a ficticious date in the future but that seems wrong
             return null;
         }
@@ -70,32 +77,54 @@ class Subscription extends Model
         $most_recent_entry = $this->getMostRecentEntry();
         if (null == $most_recent_entry) {
             // if the sub->first_entry_date is in the past or equal to now,
-            // don't have a cow, just use this date because
+            // don't have a cow, just use this date because none have been sent yet
+            // and this date is in the past, so the first email will be generated
             return $this->created_at;
         }
 
         $most_recent_entry_date = $most_recent_entry->entry_date;
+        \Log::info($most_recent_entry_date);
+        \Log::info($next_entry->entry_date);
 
         // divide by the pace
         $paced_seconds = $most_recent_entry_date->diffInSeconds($next_entry->entry_date) / $this->pace;
-        // add the result to the most recent entry date
-        $next_due_date = $most_recent_entry_date->copy()->addSeconds(round($paced_seconds));
+        \Log::info($this->pace);
+        \Log::info($paced_seconds);
+
+        // add the result to the most recent log date
+        // This is the line that MAKES THE MAGIC HAPPEN!
+        $log = $this->logs()->where('user_id', $this->user_id)->orderBy('id', 'desc')->first();
+        \Log::info($log);
+        $next_due_date = $log->created_at->copy()->addSeconds(round($paced_seconds));
+        \Log::info($next_due_date);
 
         // return as a date
         return $next_due_date;
     }
 
+    /**
+     * Returns true if an entry is past due for delivery, false otherwise
+     *
+     * @return boolean
+     */
     public function getDeliveryIsPastDueAttribute()
     {
         if (null == $this->next_entry_delivery_date) {
             return false;
         }
         // we need to conform the year part of the date or these calculations will be completely wrong
-        $now = now();
-        $next_entry_year = $this->next_entry_delivery_date->year;
-        $diffinyears = $now->year - $next_entry_year;
-        $normalized_next_entry_date = $this->next_entry_delivery_date->copy()->addYears($diffinyears);
-        return $normalized_next_entry_date < $now;
+        /**
+         * Given an entry_date of 1990-02-20
+         * And a novel that starts on 1990-06-20
+         * And ends on 2021-03-17
+         * And today's date is 2020-05-04
+         * When I check delivery_is_past_due
+         * Then the answer depends on whether or not the prior issue has been sent
+         *
+         * Examples:
+         *  - Novel starts in 2015 and ends in 2025, it is currently 2020
+         */
+        return $this->next_entry_delivery_date < now();
     }
 
     /**
@@ -109,7 +138,7 @@ class Subscription extends Model
         if (null == $most_recent_entry) {
             $next_entry = $this->novel->entries()->orderBy('entry_date')->first();
         } else {
-            $next_entry = Entry::where('novel_id', $this->novel_id)
+            $next_entry = $this->novel->entries()
                 ->where('entry_date', '>', $most_recent_entry->entry_date)
                 ->orderBy('entry_date')
                 ->first();
@@ -137,8 +166,8 @@ class Subscription extends Model
 
     public static function subscriptionsPendingDelivery()
     {
-        return array_filter(Subscription::where('status', 'active')->get()->toArray(), function ($item) {
+        return collect(array_filter(Subscription::where('status', 'active')->get()->toArray(), function ($item) {
             return (bool) $item['delivery_is_past_due'];
-        });
+        }));
     }
 }
