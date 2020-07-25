f7be74613c315e562a04e6b4dee2fd33efface68
anovelexperience <bloodbank@dracula.email>
2020-07-25 17:30:46 +0100

    Improve Subscribe Test
    Screenshot now takes place on subscribe page load and added a note about checking for a job in the jobs table (to be implemented later)

--- tests/Browser/TrialSubscriptionTest.php
+++ tests/Browser/TrialSubscriptionTest.php
@@ -43,15 +43,16 @@ class TrialSubscriptionTest extends DuskTestCase
             // should see Settings for
             $browser->loginAs($this->subscriber, 'web');
             $browser->visit(route('novel.subscribe', ['id' => $this->novel->id]))
-                ->screenshot('subscription_page')
                 ->radio('type', 'trial')
+                ->screenshot('subscription_page')
                 ->click('#trial-submit-button')
                 ->assertSee('Congratulations. You are now subscribed to')
                 ->assertSee('Settings for');
 
             // $this->subscriber->subscriptions should have the selected novel
             $this->assertTrue($this->subscriber->isSubscribed($this->novel->id));
-            // standard log should contain subscription confirmation email, or a job should be in the jobs table
+            // a job should be in the jobs table with the welcome message
+            // {"uuid":"e7710ca8-99dd-4923-8169-cf32fc870950","displayName":"App\\Jobs\\SendWelcomeMessage","job":"Illuminate\\Queue\\CallQueuedHandler@call","maxTries":null,"maxExceptions":null,"delay":null,"timeout":null,"timeoutAt":null,"data":{"commandName":"App\\Jobs\\SendWelcomeMessage","command":"O:27:\"App\\Jobs\\SendWelcomeMessage\":9:{s:4:\"user\";O:45:\"Illuminate\\Contracts\\Database\\ModelIdentifier\":4:{s:5:\"class\";s:8:\"App\\User\";s:2:\"id\";i:252;s:9:\"relations\";a:0:{}s:10:\"connection\";s:5:\"mysql\";}s:3:\"job\";N;s:10:\"connection\";N;s:5:\"queue\";N;s:15:\"chainConnection\";N;s:10:\"chainQueue\";N;s:5:\"delay\";N;s:10:\"middleware\";a:0:{}s:7:\"chained\";a:0:{}}"}}
             // activity_log should contain "New subscription for" with causer_id == user_id
         });
     }
